eaf9770e67a781a3065b14102439bd31fe057e44
GurmanDhami5 <gurmandhami01@gmail.com>
2026-06-30 21:31:42 +0530

    added piece textures and some code optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -2,192 +2,237 @@
 #include <SDL2/SDL_image.h>
 #include <array>
 #include <iostream>
-#include <string>
 
-const int SCREEN_WIDTH = 480;
-const int SCREEN_HEIGHT = 480;
+const int WINDOW_WIDTH = 480;
+const int WINDOW_HEIGHT = 480;
 const int TILE_SIZE = 60;
 const int BOARD_SIZE = 8;
 const int EMPTY = 0;
 
 struct Textures {
-    std::array<SDL_Texture *, 13> pieces{};
+    std::array<SDL_Texture *, 13> pieceTextures;
 };
 
-SDL_Window *initWindow() {
+const std::array<std::string, 13> piecesLUT = {
+    "black_king",   // index 0
+    "black_queen",  // index 1
+    "black_bishop", // index 2
+    "black_knight", // index 3
+    "black_rook",   // index 4
+    "black_pawn",   // index 5
+    "",             // index 6 (empty)
+    "white_pawn",   // index 7
+    "white_rook",   // index 8
+    "white_knight", // index 9
+    "white_bishop", // index 10
+    "white_queen",  // index 11
+    "white_king"    // index 12
+};
+
+bool initializeSDL() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError()
+        std::cerr << "SDL initialization failed! SDL Error: " << SDL_GetError()
                   << std::endl;
-        return nullptr;
+        return false;
+    }
+    return true;
+}
+
+bool initalizeSDLImage() {
+    int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        std::cerr << "SDL_Img initialization failed! SDL Error: "
+                  << SDL_GetError() << std::endl;
+        return false;
     }
 
+    return true;
+}
+
+SDL_Window *createWindow() {
     SDL_Window *window = SDL_CreateWindow("Chess",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          SCREEN_WIDTH,
-                                          SCREEN_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
+                                          WINDOW_WIDTH,
+                                          WINDOW_HEIGHT,
+                                          0);
 
     if (window == nullptr) {
-        std::cout << "Window could not be created! SDL_Error: "
+        std::cerr << "Window could not be created! SDL Error: "
                   << SDL_GetError() << std::endl;
         return nullptr;
     }
     return window;
 }
 
-SDL_Renderer *initRenderer(SDL_Window *window) {
-
+SDL_Renderer *createRenderer(SDL_Window *window) {
     SDL_Renderer *renderer =
         SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     if (renderer == nullptr) {
-        std::cout << "Renderer Error: " << SDL_GetError() << std::endl;
+        std::cerr << "Could not create renderer! SDL Error: " << SDL_GetError()
+                  << std::endl;
         return nullptr;
     }
+
     return renderer;
 }
 
-bool initSDLImage() {
-    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-        std::cout << "SDL_image Error: " << IMG_GetError() << std::endl;
-        return false;
-    }
-    return true;
+void drawBoard(SDL_Renderer *renderer);
+void drawPieces(SDL_Renderer *renderer,
+                const Textures &textures,
+                const int board[BOARD_SIZE][BOARD_SIZE]);
+
+void render(SDL_Renderer *renderer,
+            const Textures &textures,
+            const int board[BOARD_SIZE][BOARD_SIZE]) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // drawing chess board
+    drawBoard(renderer);
+    drawPieces(renderer, textures, board);
+
+    SDL_RenderPresent(renderer);
 }
 
 void drawBoard(SDL_Renderer *renderer) {
-
-    // Draw chess board
     for (int row = 0; row < BOARD_SIZE; row++) {
         for (int col = 0; col < BOARD_SIZE; col++) {
-            // Alternate colors
             if ((row + col) % 2 == 0) {
-                SDL_SetRenderDrawColor(renderer, 240, 217, 181, 255);
+
+                SDL_SetRenderDrawColor(renderer, 74, 117, 89, 255);
+
             } else {
-                SDL_SetRenderDrawColor(renderer, 181, 136, 99, 255);
+
+                SDL_SetRenderDrawColor(renderer, 240, 217, 181, 255);
             }
 
-            SDL_Rect square = {
+            SDL_Rect tile = {
                 col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE
             };
 
-            SDL_RenderFillRect(renderer, &square);
+            SDL_RenderFillRect(renderer, &tile);
         }
     }
 }
 
-int textureIndex(int piece) { return piece + 6; }
+void loadTextures(Textures &textures, SDL_Renderer *renderer) {
+    textures.pieceTextures[0] =
+        IMG_LoadTexture(renderer, "./assets/Chess_kdt60.png"); // black_king
+    textures.pieceTextures[1] =
+        IMG_LoadTexture(renderer, "./assets/Chess_qdt60.png"); // black_queen
+    textures.pieceTextures[2] =
+        IMG_LoadTexture(renderer, "./assets/Chess_bdt60.png"); // black_bishop
+    textures.pieceTextures[3] =
+        IMG_LoadTexture(renderer, "./assets/Chess_ndt60.png"); // black_knight
+    textures.pieceTextures[4] =
+        IMG_LoadTexture(renderer, "./assets/Chess_rdt60.png"); // black_rook
+    textures.pieceTextures[5] =
+        IMG_LoadTexture(renderer, "./assets/Chess_pdt60.png"); // black_pawn
 
-bool loadTexture(SDL_Renderer *renderer,
-                 Textures &textures,
-                 int piece,
-                 const std::string &path) {
-    SDL_Texture *texture = IMG_LoadTexture(renderer, path.c_str());
+    textures.pieceTextures[6] = nullptr;
 
-    if (texture == nullptr) {
-        std::cout << "Could not load " << path << ": " << IMG_GetError()
-                  << std::endl;
-        return false;
+    textures.pieceTextures[7] =
+        IMG_LoadTexture(renderer, "./assets/Chess_plt60.png"); // white_pawn
+    textures.pieceTextures[8] =
+        IMG_LoadTexture(renderer, "./assets/Chess_rlt60.png"); // white_rook
+    textures.pieceTextures[9] =
+        IMG_LoadTexture(renderer, "./assets/Chess_nlt60.png"); // white_knight
+    textures.pieceTextures[10] =
+        IMG_LoadTexture(renderer, "./assets/Chess_blt60.png"); // white_bishop
+    textures.pieceTextures[11] =
+        IMG_LoadTexture(renderer, "./assets/Chess_qlt60.png"); // white_queen
+    textures.pieceTextures[12] =
+        IMG_LoadTexture(renderer, "./assets/Chess_klt60.png"); // white_king
+}
+
+bool checkTexture(const Textures &textures) {
+    for (int i = 0; i < textures.pieceTextures.size(); i++) {
+        if (i == 6) {
+            continue;
+        }
+        if (textures.pieceTextures[i] == nullptr) {
+
+            std::cerr << "texture failed to load at index: " << piecesLUT[i]
+                      << std::endl;
+            return false;
+        }
     }
-
-    textures.pieces[textureIndex(piece)] = texture;
     return true;
 }
 
-bool loadPieceTextures(SDL_Renderer *renderer, Textures &textures) {
-    return loadTexture(renderer, textures, 1, "assets/Chess_plt60.png") &&
-           loadTexture(renderer, textures, 2, "assets/Chess_rlt60.png") &&
-           loadTexture(renderer, textures, 3, "assets/Chess_nlt60.png") &&
-           loadTexture(renderer, textures, 4, "assets/Chess_blt60.png") &&
-           loadTexture(renderer, textures, 5, "assets/Chess_qlt60.png") &&
-           loadTexture(renderer, textures, 6, "assets/Chess_klt60.png") &&
-           loadTexture(renderer, textures, -1, "assets/Chess_pdt60.png") &&
-           loadTexture(renderer, textures, -2, "assets/Chess_rdt60.png") &&
-           loadTexture(renderer, textures, -3, "assets/Chess_ndt60.png") &&
-           loadTexture(renderer, textures, -4, "assets/Chess_bdt60.png") &&
-           loadTexture(renderer, textures, -5, "assets/Chess_qdt60.png") &&
-           loadTexture(renderer, textures, -6, "assets/Chess_kdt60.png");
-}
-
-void destroyTextures(Textures &textures) {
-    for (SDL_Texture *texture : textures.pieces) {
-        if (texture != nullptr) {
-            SDL_DestroyTexture(texture);
-        }
-    }
-}
+int pieceToIndex(int piece) { return piece + 6; }
 
 void drawPieces(SDL_Renderer *renderer,
-                const int board[BOARD_SIZE][BOARD_SIZE],
-                const Textures &textures) {
+                const Textures &textures,
+                const int board[BOARD_SIZE][BOARD_SIZE]) {
     for (int row = 0; row < BOARD_SIZE; row++) {
         for (int col = 0; col < BOARD_SIZE; col++) {
+
             int piece = board[row][col];
 
             if (piece == EMPTY) {
                 continue;
             }
 
-            SDL_Texture *texture = textures.pieces[textureIndex(piece)];
-
-            if (texture == nullptr) {
-                continue;
-            }
-
-            SDL_Rect destination = {
+            SDL_Rect pieceRect = {
                 col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE
             };
 
-            SDL_RenderCopy(renderer, texture, nullptr, &destination);
+            SDL_Texture *pieceTexture =
+                textures.pieceTextures[pieceToIndex(piece)];
+
+            if (pieceTexture == nullptr) {
+                continue;
+            }
+
+            SDL_RenderCopy(renderer, pieceTexture, nullptr, &pieceRect);
         }
     }
 }
 
-void render(SDL_Renderer *renderer,
-            const int board[BOARD_SIZE][BOARD_SIZE],
-            const Textures &textures) {
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    drawBoard(renderer);
-    drawPieces(renderer, board, textures);
-
-    SDL_RenderPresent(renderer);
+void destroyTextures(Textures &textures) {
+    for (SDL_Texture *texture : textures.pieceTextures) {
+        if (texture != nullptr) {
+            SDL_DestroyTexture(texture);
+        }
+    }
 }
 
-void initializeBoard(int board[BOARD_SIZE][BOARD_SIZE]) {
+void initalizeBoard(int board[BOARD_SIZE][BOARD_SIZE]) {
     for (int row = 0; row < BOARD_SIZE; row++) {
         for (int col = 0; col < BOARD_SIZE; col++) {
-            board[row][col] = EMPTY;
+            if (row == 1) {
+                board[row][col] = -1;
+            } else if (row == 6) {
+                board[row][col] = 1;
+            } else {
+                board[row][col] = EMPTY;
+            }
         }
     }
 
-    for (int col = 0; col < BOARD_SIZE; col++) {
-        board[1][col] = -1;
-        board[6][col] = 1;
-    }
+    // black pieces
+    board[0][0] = -2; // b_rook
+    board[0][1] = -3; // b_knight
+    board[0][2] = -4; // b_bishop
+    board[0][3] = -5; // b_queen
+    board[0][4] = -6; // b_king
+    board[0][5] = -4; // b_bishop
+    board[0][6] = -3; // b_knight
+    board[0][7] = -2; // b_rook
 
-    // Black back row
-    board[0][0] = -2; // rook
-    board[0][1] = -3; // knight
-    board[0][2] = -4; // bishop
-    board[0][3] = -5; // queen
-    board[0][4] = -6; // king
-    board[0][5] = -4; // bishop
-    board[0][6] = -3; // knight
-    board[0][7] = -2; // rook
-
-    // White back row
-    board[7][0] = 2; // rook
-    board[7][1] = 3; // knight
-    board[7][2] = 4; // bishop
-    board[7][3] = 5; // queen
-    board[7][4] = 6; // king
-    board[7][5] = 4; // bishop
-    board[7][6] = 3; // knight
-    board[7][7] = 2; // rook
+    // white pieces
+    board[7][0] = 2; // w_rook
+    board[7][1] = 3; // w_knight
+    board[7][2] = 4; // w_bishop
+    board[7][3] = 5; // w_queen
+    board[7][4] = 6; // w_king
+    board[7][5] = 4; // w_bishop
+    board[7][6] = 3; // w_knight
+    board[7][7] = 2; // w_rook
 }
 
 void printBoard(const int board[BOARD_SIZE][BOARD_SIZE]) {
@@ -195,22 +240,31 @@ void printBoard(const int board[BOARD_SIZE][BOARD_SIZE]) {
         for (int col = 0; col < BOARD_SIZE; col++) {
             std::cout << board[row][col] << "\t";
         }
-        std::cout << '\n';
+        std::cout << std::endl;
     }
 }
 
 int main() {
 
     int board[BOARD_SIZE][BOARD_SIZE];
-    initializeBoard(board);
+    initalizeBoard(board);
 
-    SDL_Window *window = initWindow();
-
-    if (!window) {
+    if (!initializeSDL()) {
         return 1;
     }
 
-    SDL_Renderer *renderer = initRenderer(window);
+    if (!initalizeSDLImage()) {
+        return 1;
+    }
+
+    SDL_Window *window = createWindow();
+
+    if (!window) {
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = createRenderer(window);
 
     if (!renderer) {
         SDL_DestroyWindow(window);
@@ -218,15 +272,9 @@ int main() {
         return 1;
     }
 
-    if (!initSDLImage()) {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
     Textures textures;
-    if (!loadPieceTextures(renderer, textures)) {
+    loadTextures(textures, renderer);
+    if (!checkTexture(textures)) {
         destroyTextures(textures);
         IMG_Quit();
         SDL_DestroyRenderer(renderer);
@@ -235,20 +283,19 @@ int main() {
         return 1;
     }
 
-    bool quit = false;
+    bool isRunning = true;
     SDL_Event e;
 
-    while (!quit) {
+    while (isRunning) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
-                quit = true;
+                isRunning = false;
             }
         }
-        render(renderer, board, textures);
+        render(renderer, textures, board);
     }
 
     destroyTextures(textures);
-    IMG_Quit();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
